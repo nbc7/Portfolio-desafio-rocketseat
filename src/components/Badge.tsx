936b604727f6bbac2a2cd3a78c0a32cc6a7967ba
nbc7 <90021751+nbc7@users.noreply.github.com>
2022-07-26 17:53:30 -0300

    technologies card

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,7 +1,11 @@
-export function Badge(props: { text: string }) {
+export function Badge(props: { text: string; icon: React.ReactNode }) {
   return (
-    <div className="h-6 w-[86px] bg-lightpurple-100 rounded-[30px] uppercase text-black font-bold text-[10px] leading-[13px] text-center pt-[6px]">
-      {props.text}
+    <div className="group">
+      {/* <div className="invisible hover:visible h-6 w-[86px] bg-lightpurple-100 rounded-[30px] uppercase text-black font-bold text-[10px] leading-[13px] text-center pt-[6px]"> */}
+      <div className="invisible group-hover:visible absolute -ml-[17px] -mt-7 h-6 w-[86px] bg-lightpurple-100 rounded-[30px] text-black font-bold text-[10px] leading-[13px] text-center pt-[6px]">
+        {props.text}
+      </div>
+      {props.icon}
     </div>
   );
 }
